71bedaa90c10d839758a6fe67b022ce9bc259db0
dutt26 <gsyashwanthdutt26@gmail.com>
2025-12-09 13:01:06 +0530

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "clone-uber-bucket" # Replace with your actual S3 bucket name
-    key    = "EKS/terraform.tfstate"
+    key    = "EKS_terraform.tfstate"
     region = "us-east-1"
   }
 }
